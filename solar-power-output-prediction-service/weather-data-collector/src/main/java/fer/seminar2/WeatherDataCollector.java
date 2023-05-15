cad504edccc13d79fda3c75bc2e1bf2858988d19
BernardKazazic <92548057+BernardKazazic@users.noreply.github.com>
2023-05-15 23:30:34 +0200

    renamed main and added spring

---
+++ solar-power-output-prediction-service/weather-data-collector/src/main/java/fer/seminar2/WeatherDataCollector.java
@@ -0,0 +1,10 @@
+package fer.seminar2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataCollector {
+    public static void main(String[] args) {
+
+    }
+}
